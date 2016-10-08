3f66678541dafa7054264c4f5fe06bc498ac53a3
x38z <x38z@nordstrom.com>
2016-10-07 21:44:08 -0600

    Update run_analysis.R
    R program to clean Samsung data dataset and aggregate by activity type.

--- run_analysis.R
+++ run_analysis.R
@@ -4,22 +4,23 @@ fileUrl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20
 download.file(fileUrl,destfile="Dataset.zip")
 unzip(zipfile="Data.zip",exdir=".")
 }
-
+#parsing observation files in test and train directories 
 filelist_obs <- list.files(path = "C:/Users/x38z/Documents/UCI HAR Dataset", recursive = TRUE ,pattern= "^x_t(.*)txt" ,ignore.case=TRUE,full.names=TRUE )
 
-#assuming tab separated values with a header    
+ 
 datalist_obs <-  lapply(filelist_obs, function(x)read.table(x, header=F)) 
 
-#assuming the same header/columns for all files
+#assuming the same columns for all files
 datafr_obs <- do.call("rbind", datalist_obs) 
 
+#parsing activities files in test and train directories
 filelist_activities <- list.files(path = "C:/Users/x38z/Documents/UCI HAR Dataset", recursive = TRUE ,pattern= "^y_t(.*)txt" ,ignore.case=TRUE,full.names=TRUE )
 
 datalist_activities<-  lapply(filelist_activities, function(x)read.table(x, header=F))
 
 datafr_act<- do.call("rbind", datalist_activities)
 
-
+#parsing column names from features file and setting meaningful column names
 features <- read.table("C:/Users/x38z/Documents/UCI HAR Dataset/features.txt")
 
 clean_names=as.data.frame(clean_names)
@@ -35,4 +36,5 @@ colnames(datafr_act)<- c("activity")
 ## selecting only mean and std columns
 
 cleanfr<- cbind(select(datafr_obs,contains("mean"),contains("std")),datafr_act)
+  #aggreated dataset by activity
 final<-aggregate(x=cleanfr,by=list(cleanfr[,87]),FUN="mean")
