d57d54afd719dfbd5251ca3e0f4c440e2daa7445
x38z <x38z@nordstrom.com>
2016-10-14 18:26:08 -0600

    Update run_analysis.R
    This file would open UCI HAR dataset ( Human Activity Recognition Using Smartphones Dataset.
    The dataset includes the following files:
    =========================================
    
    - 'README.txt'
    
    - 'features_info.txt': Shows information about the variables used on the feature vector.
    
    - 'features.txt': List of all features.
    
    - 'activity_labels.txt': Links the class labels with their activity name.
    
    - 'train/X_train.txt': Training set.
    
    - 'train/y_train.txt': Training labels.
    
    - 'test/X_test.txt': Test set.
    
    - 'test/y_test.txt': Test labels.
    The following files are available for the train and test data. Their descriptions are equivalent. 
    
    - 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
    
    The process would merge needed information from test, training set for observation , activities and subjects.
    It would calculate avarage data for all MEAN and STD columns.

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,45 @@
 library(dplyr)
 if(!file.exists("./UCI HAR Dataset")){
-fileUrl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="Dataset.zip")
+  fileUrl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl,destfile="Dataset.zip")
 unzip(zipfile="Data.zip",exdir=".")
 }
-#parsing observation files in test and train directories 
-filelist_obs <- list.files(path = "C:/Users/x38z/Documents/UCI HAR Dataset", recursive = TRUE ,pattern= "^x_t(.*)txt" ,ignore.case=TRUE,full.names=TRUE )
-
- 
-datalist_obs <-  lapply(filelist_obs, function(x)read.table(x, header=F)) 
-
-#assuming the same columns for all files
-datafr_obs <- do.call("rbind", datalist_obs) 
-
-#parsing activities files in test and train directories
-filelist_activities <- list.files(path = "C:/Users/x38z/Documents/UCI HAR Dataset", recursive = TRUE ,pattern= "^y_t(.*)txt" ,ignore.case=TRUE,full.names=TRUE )
-
-datalist_activities<-  lapply(filelist_activities, function(x)read.table(x, header=F))
-
-datafr_act<- do.call("rbind", datalist_activities)
-
-#parsing column names from features file and setting meaningful column names
-features <- read.table("C:/Users/x38z/Documents/UCI HAR Dataset/features.txt")
-
-clean_names=as.data.frame(clean_names)
-clean_names<- tolower(gsub("\\(|\\)|\\,","",(gsub("-","_",features[[2]]))))
-clean_names=as.data.frame(clean_names)
-#Adding index for duplicate names
-clean_names_ind<- paste(clean_names$clean_names,sep="_", seq.int(nrow(clean_names)))
 
 
-colnames(datafr_obs)<- clean_names_ind
-colnames(datafr_act)<- c("activity")
+
+obs_train<- read.table("./UCI HAR Dataset/train/X_train.txt")
+obs_test<-read.table("./UCI HAR Dataset/test/x_test.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subject_test<- read.table("./UCI HAR Dataset/test/subject_test.txt")
+activity_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
+activity_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
+
+
+
+
+
+
+features <- read.table("C:/Users/x38z/Documents/UCI HAR Dataset/features.txt")[,2]
+extract_features <- grepl("mean|std", features)
+
+
+
+
+colnames(obs_train)<- features
+colnames(obs_test)<- features
+colnames(activity_train)<- c("activity")
+colnames(activity_test)<- c("activity")
+colnames(subject_train)<- c("subject")
+colnames(subject_test)<- c("subject")
 
 ## selecting only mean and std columns
 
-cleanfr<- cbind(select(datafr_obs,contains("mean"),contains("std")),datafr_act)
-  #aggreated dataset by activity
-final<-aggregate(x=cleanfr,by=list(cleanfr[,87]),FUN="mean")
+cleanfr_train<- cbind(obs_train[extract_features],activity_train,subject_train)
+cleanfr_test<- cbind(obs_test[extract_features],activity_test,subject_test)
+
+# merging all
+
+cleanfr<- rbind(cleanfr_train,cleanfr_test)
+ final<-aggregate(x=cleanfr,by=list(cleanfr$activity,cleanfr$subject),FUN="mean")
+
+ write.table(final , file = "./clean_data.txt")
